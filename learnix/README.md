d3ec96ab5b766ba4c98eedd817b780e90f5e7dd2
A-janjan <a.m.x.janjan@gmail.com>
2024-04-29 17:01:24 +0330

    add cache framework

--- learnix/README.md
+++ learnix/README.md
@@ -2,3 +2,18 @@
 
 -> E-Learning Platform
 
+## some commands
+
+
+for caching system :
+
+`docker pull memcached`
+
+and:
+
+`docker run -it --rm --name memcached -p 11211:11211 memcached -m 64`
+
+
+or for Redis:
+
+`docker run -it --rm --name redis -p 6379:6379 redis`
--- learnix/learnix/learnix/settings.py
+++ learnix/learnix/learnix/settings.py
@@ -42,13 +42,16 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'students.apps.StudentsConfig',
     'embed_video',
+    'redisboard',
 ]
 
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    # 'django.middleware.cache.UpdateCacheMiddleware',
     "django.middleware.common.CommonMiddleware",
+    # 'django.middleware.cache.FetchFromCacheMiddleware',
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -137,3 +140,16 @@ INTERNAL_IPS = [
 
 
 LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
+    }
+}
+
+
+CACHE_MIDDLEWARE_ALIAS = 'default'
+CACHE_MIDDLEWARE_SECONDS = 60 * 15 # 15 minutes
+CACHE_MIDDLEWARE_KEY_PREFIX = 'learnix'
